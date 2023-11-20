82fe5099b91869690d0831acb73653e3ca996e07
dhruv saini <dhruvsaini1997@gmail.com>
2023-11-20 17:39:21 -0500

    third  commit

--- Database/index.js
+++ Database/index.js
@@ -1,24 +1,15 @@
-import assert from 'assert';
-import courses from './courses.json';
-import modules from './modules.json';
-import assignments from './assignments.json';
-import users from './users.json';
-import enrollments from './enrollments.json';
-import grades from './grades.json';
-
-// Asserting that the imported files are of type 'json'
-assert.strictEqual(typeof courses, 'object', 'courses.json is not a valid JSON file');
-assert.strictEqual(typeof modules, 'object', 'modules.json is not a valid JSON file');
-assert.strictEqual(typeof assignments, 'object', 'assignments.json is not a valid JSON file');
-assert.strictEqual(typeof users, 'object', 'users.json is not a valid JSON file');
-assert.strictEqual(typeof enrollments, 'object', 'enrollments.json is not a valid JSON file');
-assert.strictEqual(typeof grades, 'object', 'grades.json is not a valid JSON file');
+import courses from "./courses.json" assert { type: "json" };
+import modules from "./modules.json" assert { type: "json" };
+import assignments from "./assignments.json" assert { type: "json" };
+// import users from "./users.json" assert { type: "json" };
+// import grades from "./grades.json" assert { type: "json" };
+// import enrollments from "./enrollments.json" assert { type: "json" };
 
 export default {
   courses,
   modules,
   assignments,
-  users,
-  enrollments,
-  grades,
+  // users,
+  // grades,
+  // enrollments,
 };
